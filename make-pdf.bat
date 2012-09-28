e7379769ab28bbec74cd7da1807d775731e9b074
Erik Wilde <dret@users.noreply.github.com>
2012-09-28 18:50:19 +0000

    added WoT 2012 proceedings

--- make-pdf.bat
+++ make-pdf.bat
@@ -1,9 +1,9 @@
 pdflatex dretbiblio
-bibtex8 --wolfgang dretbiblio
+bibtex dretbiblio
 pdflatex dretbiblio
-bibtex8 --wolfgang dretbiblio
+bibtex dretbiblio
 pdflatex dretbiblio
-bibtex8 --wolfgang dretbiblio
+bibtex dretbiblio
 pdflatex dretbiblio
 perl authorindex.pl dretbiblio
 pdflatex dretbiblio
